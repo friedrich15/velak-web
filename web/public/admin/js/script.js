4187b72b83c58ef04fd69a6cc30fdf55282afc4d
friedrich15 <emdo2000@gmail.com>
2015-07-27 17:49:01 +0200

    Added info/media-tabs.

--- web/public/admin/js/script.js
+++ web/public/admin/js/script.js
@@ -1,6 +1,13 @@
 $(function(){
   append_hover();
+
+  // $('#tab-menu a').click(function (e) {
+  //   // e.preventDefault();
+  //   $(this).tab('show');
+  // });
 });
+
+
 
 function append_hover() {
   $( "li.li-project" ).hover(
