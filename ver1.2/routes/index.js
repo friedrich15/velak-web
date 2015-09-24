61eaed88126c83a62af391aa644addc13fc64b3a
friedrich15 <emdo2000@gmail.com>
2015-09-24 19:21:55 +0200

    main page: loading projects is working.

--- ver1.2/routes/index.js
+++ ver1.2/routes/index.js
@@ -5,10 +5,48 @@ var Project  = mongoose.model('Project');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Project.find().sort('position').exec(function(err, projects){
+    res.render('index', {
+      title: 'velak',
+      projects: projects
+    });
+  ;});
+});
+router.get('/gala/:id*?', function(req, res, next) {
+  console.log('gala');
+  Project.find({category: 'gala'}).sort('position').exec(function(err, projects){
+    res.render('site', {
+      title: 'velak',
+      site: 'gala',
+      pId: req.params.id,
+      projects: projects
+    });
+  ;});
+});
+
+router.get('/export', function(req, res, next) {
+  console.log('export');
+  Project.find({category: 'export'}).sort('position').exec(function(err, projects){
+    res.render('site', {
+      title: 'velak',
+      site: 'export',
+      projects: projects
+    });
+  ;});
+});
+
+router.get('/other', function(req, res, next) {
+  console.log('other');
+  Project.find({category: 'other'}).sort('position').exec(function(err, projects){
+    res.render('other', {
+      title: 'velak',
+      projects: projects
+    });
+  ;});
 });
 
 
+// PHOTOLINK
 router.get('/photolink/:id/:rndm', function(req, res, next) {
   Project.findById(req.params.id, function(err, project) {
 
